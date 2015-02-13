b7a191495210bd0496295c8dbb020895ffd168f5
ky23 <khatal.yacine@gmail.com>
2015-02-13 20:07:24 +0100

    server: create user controller

---
+++ api/controllers/UserController.js
@@ -0,0 +1,16 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	signup: function (req, res) {
+        
+    },
+    login: function (req, res) {
+        
+    }
+};
+
