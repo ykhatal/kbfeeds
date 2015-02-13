b78d2abdddf6a3937428d2bde54cb073b04be701
ky23 <khatal.yacine@gmail.com>
2015-02-13 20:07:08 +0100

    server: create main controller

---
+++ api/controllers/MainController.js
@@ -0,0 +1,13 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing mains
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function (req, res) {
+        res.view();
+    }
+};
+
