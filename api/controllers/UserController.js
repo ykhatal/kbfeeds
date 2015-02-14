9ccd0f39ba37d2bf469ce16d7dc5f838afe7857d
ky23 <khatal.yacine@gmail.com>
2015-02-14 04:12:57 +0100

    multiple changes 	- add login logic 	- add signup logic

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,10 +7,79 @@
 
 module.exports = {
 	signup: function (req, res) {
-        
+        var bcrypt = require('bcrypt');
+        if (utils.checkEmail(req.body.username)) {
+      		if (utils.checkPassword(req.body.password)) {
+        		if (utils.checkSamePasswords(req.body.password, req.body.repeat_password)) {
+        			User.findOneByUsername(req.body.username).exec(function (err, user) {
+	      				if (err) {
+				      		return res.status(500).json({ error: 'DB error' });
+				      	}
+	      				if (!user) {
+		            		User.create({ username: req.body.username, password: req.body.password }).
+		            		exec(function(error, user) {
+					        	if(error) {
+					            	return res.status(500).json({ error: 'DB error' });
+					        	} else {
+					            	return res.status(200).json({ success: 'User was successfully added' });
+					        	}
+					    	});
+		          		} else {
+		            		return res.status(404).json({ error: 'Username already exist' });
+		          		}
+	        		});
+        		} else {
+        			return res.status(403).json({ error: 'Passwords don\'t match' });
+        		}
+    		} else {
+    			return res.status(403).json({ error: 'password is in an invalid format, must contain at least one'
+           		+ 'digit/lowercase/uppercase letter and be at least six characters long' });
+    		}
+		} else {
+			return res.status(403).json({ error: 'Username invalid, must be like : email@example.com' });
+		}
     },
+
     login: function (req, res) {
-        
-    }
+        var bcrypt = require('bcrypt');
+        if (utils.checkGrantType(req.body.grant_type, 'password')) {
+        	User.findOneByUsername(req.body.username).exec(function (err, user) {
+		      	if (err) {
+		      		return res.status(500).json({ error: 'DB error' });
+		      	} 
+		      	if (user) {
+		        	bcrypt.compare(req.body.password, user.password, function (err, match) {
+			          	if (err) {
+			          		return res.status(500).json({ error: 'Server error' });
+			          	} 
+			          	if (match) {
+			            	// password match
+			            	var token = User.createTokenObject('Bearer');
+			            	user.token_type = token.token_type;
+			            	user.access_token = token.access_token;
+			            	user.refresh_token = token.refresh_token;
+			            	user.expire_in = token.expire_in;
+			            	user.save(function(error) {
+						        if(error) {
+						            return res.status(500).json({ error: 'DB error' });
+						        } else {
+						            return res.status(200).json({ success: {
+					            		username: user.username,
+					            		token: token,
+			            			} });
+						        }
+						    });
+			          	} else {
+			            	res.status(404).json({ error: 'Invalid username or password' });
+			          	}
+		        	});
+		      	} else {
+		        	res.status(404).json({ error: 'Invalid username or password' });
+		      	}
+    		});
+        } else {
+        	return res.status(403).json({ error: 'grant_type don\'t match' });
+        }
+  	}
 };
 
