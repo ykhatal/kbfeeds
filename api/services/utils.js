ef4ee186ebbc05bd06e029b8eded6d66a80addba
ky23 <khatal.yacine@gmail.com>
2015-02-14 04:17:46 +0100

    server: add utils service

---
+++ api/services/utils.js
@@ -0,0 +1,37 @@
+/**
+* utils.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+var validator = require('validator');
+
+module.exports = {
+    checkEmail: function(email) {
+        return validator.isEmail(email);
+    },
+    checkPassword: function(password) {
+        var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        return re.test(password);
+    },
+    checkTokenValidity: function(date) {
+        return validator.isAfter(date);
+    },
+    checkSamePasswords: function(password, repeatPassword) {
+        return (password === repeatPassword);
+    },
+    checkGrantType: function(reqType, grantType) {
+        return (reqType === grantType);
+    },
+    generateToken: function() {
+        var crypto = require('crypto');
+        var key = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for(var i = 0; i < 8; i++) {
+            key += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return crypto.createHash('sha256').update(key).digest('hex');
+    }
+};
+
