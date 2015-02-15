0fbde793ac572ce3471ce86b70bbf60c476859e7
ky23 <khatal.yacine@gmail.com>
2015-02-15 03:04:46 +0100

    create feed model

---
+++ api/models/Feed.js
@@ -0,0 +1,22 @@
+/**
+* Feed.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	// Database connection
+	connection: 'kbfeedsMongodb',
+
+	// Must follow table Schema
+	schema: true,
+
+	// Feed table schema
+	attributes: {
+	    name: { type: 'string' },
+	    url: { type: 'string' },
+	  	owner: { model: 'user' }
+	}
+};
+
