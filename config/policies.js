986440e95ff05e66615f64953db34d11488e0372
ky23 <khatal.yacine@gmail.com>
2015-02-15 03:02:41 +0100

    - add policies (isPost, isGet, isDelete, isPut check respectively if http request method is POST, GET, DELETE, PUT)  - add policies OAuth to check if request is authenticated

--- config/policies.js
+++ config/policies.js
@@ -34,18 +34,14 @@ module.exports.policies = {
   * and its actions                                                          *
   *                                                                          *
   ***************************************************************************/
-	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
+  UserController: {
+      login: 'isPost',
+      signup: 'isPost'
+  },
+  FeedController: {
+      index: ['isGet', 'OAuth'],
+      add: ['isPost', 'OAuth'],
+      delete: ['isDelete', 'OAuth'],
+      getById: ['isGet', 'OAuth']
+  }
 };
