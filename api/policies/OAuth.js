610fe656292d699edae55d964945597a3f785a05
ky23 <khatal.yacine@gmail.com>
2015-02-15 03:06:19 +0100

    create OAuth middleware

---
+++ api/policies/OAuth.js
@@ -0,0 +1,30 @@
+/**
+ * OAuth
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any authenticated query
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+module.exports = function(req, res, next) {
+	var authorization = req.headers.authorization;
+    if (!authorization) {
+    	return res.status(404).json({ error: 'Missing authorization header' });
+    }
+    authorization = authorization.toString().split(' ');
+	if (utils.checkTokenType(authorization[0], 'Bearer')) {
+		User.findOneByAccessToken(authorization[1]).populate('feeds').exec(function (err, user) {
+			if (err) {
+				return res.status(500).json({ error: 'DB error' });
+			}
+			if (user) {
+				req.user = user;
+				return next();
+			} else {
+  				return res.status(404).json({ error: 'Wrong token type or access token' });
+			}
+		});
+  	} else {
+  		return res.status(404).json({ error: 'Wrong token type or access token' });
+  	}
+};
