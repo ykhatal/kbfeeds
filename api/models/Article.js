870ed97d0a298ef8b01bc31e1d3ad1cc391e21a6
ky23 <khatal.yacine@gmail.com>
2015-02-18 18:36:52 +0100

    add article model

---
+++ api/models/Article.js
@@ -0,0 +1,24 @@
+/**
+* Article.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	// Database connection
+	connection: 'kbfeedsMongodb',
+
+	// Must follow table Schema
+	schema: true,
+
+	// Category table schema
+	attributes: {
+  		name: { type: 'string' },
+  		link: {type: 'string' },
+  		owner: { model: 'user' }
+	}
+};
+
+
+
